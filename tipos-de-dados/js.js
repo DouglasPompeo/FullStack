29fecd4e094f4fb1ae296f9a3426f8261b61f864
DouglasPompeo01~ <dpompeo20@gmail.com>
2023-07-14 15:01:31 -0300

    atualizado

--- tipos-de-dados/js.js
+++ tipos-de-dados/js.js
@@ -3,3 +3,7 @@ var sobrenome = "Aguiar Fernandes"
 var nomeCompleto = nome + " " + sobrenome;
 var vazio = ""
 console.log("nomeCompleto" = + nomeCompleto + vazio);
+
+var numeroString = "10" + 20;
+var numeroStringInv = "20" + 10;
+console.log("valor :" + numeroString + "-" + numeroStringInv)
