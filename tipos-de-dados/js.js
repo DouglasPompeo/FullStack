786c9d621d14c8a2496f7794d3a8b6a817707f8f
DouglasPompeo01~ <dpompeo20@gmail.com>
2023-07-17 14:53:09 -0300

    atualizado

--- tipos-de-dados/js.js
+++ tipos-de-dados/js.js
@@ -7,3 +7,11 @@ console.log("nomeCompleto" = + nomeCompleto + vazio);
 var numeroString = "10" + 20;
 var numeroStringInv = "20" + 10;
 console.log("valor :" + numeroString + "-" + numeroStringInv)
+
+var stringvar = "string text"
+var numerovar = 10;
+
+console.log("tipo 1 :" + typeof stringvar);
+console;log("tipo 2 :" + typeof numerovar);
+
+console.log("tamanho string :" + stringvar.length);
