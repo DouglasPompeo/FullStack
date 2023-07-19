d0c921cea13b8ed0b31837b06a7f9ae926724983
DouglasPompeo01~ <dpompeo20@gmail.com>
2023-07-19 16:19:46 -0300

    atualizado

---
+++ tipos-de-dados/string.js
@@ -0,0 +1,10 @@
+var idade = 20 ;
+console.log(idade);
+
+var preçofeijao = 2.89;
+console.log(preçofeijao);
+console.log(typeof preçofeijao);
+
+var valor = 10 + 10;
+console.log(valor);
+console.log(typeof valor);
