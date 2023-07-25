0618b8a2f832fe0ad7ff2428b242724e035077e7
DouglasPompeo01~ <dpompeo20@gmail.com>
2023-07-25 15:23:16 -0300

    atualizado

---
+++ tipos-de-dados/array.js
@@ -0,0 +1,16 @@
+var produtos = {};
+var produtos2 = ['Produto A' , 'Produto B' , 'Produto B'];
+var valores = ['Pedro' , 'Maria' , 45 , true ,{nome : Antonio}];
+
+var array = new array ();
+var array2 = new array ('A', 23 , false); 
+
+console.log(array2[0]);
+console.log(array2[2]);
+console.log(array2[1]);
+console.log(produtos2[0]);
+console.log(produtos2[2]);
+console.log(produtos2[2].nome);
+console.log(valores2[0]);
+console.log(valores2[2]);
+console.log(valores2[4]);
