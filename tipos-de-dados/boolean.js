1362f89aba3f7b342238661a88407cdb13f89f8d
DouglasPompeo01~ <dpompeo20@gmail.com>
2023-07-25 14:46:47 -0300

    atualizado

---
+++ tipos-de-dados/boolean.js
@@ -0,0 +1,12 @@
+var contastatus = false;
+console.log("status da conta :" , contastatus);
+
+var contastatus = true;
+console.log("status da conta :" , contastatus);
+
+        if(contastatus == true){//true
+        
+        console.log("Enviar email!")
+        }else{
+        console.log("ative sua conta")
+        }
---
+++ tipos-de-dados/object.js
@@ -0,0 +1,13 @@
+var itens= {};
+var itens2 = {nome : "refrigerante" , preço : "10" , ativo : "true" , detalhes : {detalhe: '...'}};
+
+var pessoa = new object();
+Pessoa.nome = "João"
+Pessoa.idade = 20;
+
+console.log(itens2.nome);
+console.log(itens2.preço);
+console.log(itens2['ativo']);
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(itens2.detalhes.detalhe);
